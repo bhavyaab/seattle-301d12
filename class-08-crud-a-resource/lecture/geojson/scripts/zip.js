71786531b75c977e0c7cecc8f3035d85c43d58ce
Bhavya Bharti <bhavyab@outlook.com>
2016-09-21 15:58:57 -0700

    create data table sql

--- class-08-crud-a-resource/lecture/geojson/scripts/zip.js
+++ class-08-crud-a-resource/lecture/geojson/scripts/zip.js
@@ -9,11 +9,15 @@
 
   function createTable() {
     webDB.execute(
-      
+      'CREATE TABLE IF NOT EXIST zips( '+'id INTEGER PRIMARY KEY,'+ ' zip INTEGER,'+ 'neighborhood VARCHAR,'+ 'county VARCHAR'+');',
+      function(){
+        console.log('SUCCESSFULLY SETUP THE ARTICLE TABLE');
+      }
     );
   };
 
   Zip.fetchAll = function() {
+    createTable();
     $.getJSON('/data/manhattan.json', function(data) {
       data.features.map(function(bigDataSet) {
         return bigDataSet.properties;
@@ -30,7 +34,7 @@
     webDB.execute(
       [
         {
-
+          'sql':'INSERT INTO zips(zip, neighborhood, county) VALUES(?,?,?);',
           'data': [this.zip, this.neighborhood, this.county]
         }
       ]
